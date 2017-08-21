e6d7648f1d59395881eb8e0891dd29f0b1632b4d
gvilauy <gabrielvila13@gmail.com>
2017-08-21 14:23:56 -0300

    Initial commit

---
+++ src/org/xpande/dibal/model/Init.java
@@ -0,0 +1,9 @@
+package org.xpande.dibal.model;
+
+/**
+ * Product: Adempiere ERP & CRM Smart Business Solution. Localization : Uruguay - Xpande
+ * Xpande. Created by Gabriel Vila on 8/21/17.
+ */
+public class Init {
+
+}
